28a4f8e9c7ebe0ae9436fd5b47e0ed310c90882e
JacobGiordano <jacob.giordano12@gmail.com>
2020-12-14 13:12:07 -0800

    Add single operations for multi-digit numbers

--- js/javascript.js
+++ js/javascript.js
@@ -10,21 +10,16 @@ const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 const operate = (operator, a, b) => {
   switch (true) {
-    case (operator === "add" ):
+    case (operator === "+" ):
       return add(a, b);
-      break;
-    case (operator === "subtract" ):
+    case (operator === "-" ):
       return subtract(a, b);
-      break;
-    case (operator === "multiply" ):
+    case (operator === "x" ):
       return multiply(a, b);
-      break;
-    case (operator === "divide" ):
+    case (operator === "/" ):
       return divide(a, b);
-      break;
     default:
       result;
-      break;
   }
 }
 
@@ -33,41 +28,85 @@ let b;
 let result;
 let clickedFunction;
 const readoutEl = document.getElementById("calc-read-out");
+const tape = document.getElementById("calc-tape");
 
 // on click
+
 document.addEventListener("click", (e) => {
   let clickedEl = e.target;
-  // console.log(clickedEl);
+  let clickedValue = clickedEl.getAttribute("data-value");
   if (clickedEl.classList.contains("calc-num-btn")) {
-    let clickedNumVal = Number(clickedEl.getAttribute("data-value"));
-    if (!isNaN(clickedNumVal)) {
-      // console.log(clickedNumVal);
-      result = clickedNumVal;
-      // console.log(result);
-      readoutEl.value = result;
-      if (a != undefined) {
-        b = clickedNumVal;
-        result = operate(clickedFunction, a, b);
-      }
+    readoutEl.value += clickedValue;
+    if (clickedFunction !== undefined && b === undefined) {
+      readoutEl.value = clickedValue;
+      b = readoutEl.value;
     }
   }
   if (clickedEl.classList.contains("calc-operation-btn")) {
-    a = result;
-    console.log(result);
-    clickedFunction = e.target.getAttribute("data-value");
-    console.log(clickedFunction);
-    if (clickedFunction == "clear") {
-      result = 0;
-      readoutEl.value = result;
-      a = undefined;
-      b = undefined;
-    }
-    if (a !== undefined && b !== undefined) {
-      readoutEl.value = result;
-      if (clickedFunction == "equals") {
-        a = undefined;
-        b = undefined;
-      }
-    }
+    a = Number(readoutEl.value);
+    clickedFunction = clickedValue;
+  }
+  if (clickedEl.classList.contains("equals-btn")) {
+    b = Number(readoutEl.value);
+    result = operate(clickedFunction, a, b);
+    readoutEl.value = result;
+    console.log(`${a} ${clickedFunction} ${b} = ${result}`);
+  }
+  if (clickedValue === "clear") {
+    a = undefined;
+    b = undefined;
+    clickedFunction = undefined;
+    readoutEl.value = "";
   }
 });
+
+
+
+
+
+// document.addEventListener("click", (e) => {
+//   let clickedEl = e.target;
+//   // console.log(clickedEl);
+//   if (clickedEl.classList.contains("calc-num-btn")) {
+//     let clickedNumVal = Number(clickedEl.getAttribute("data-value"));
+//     if (!isNaN(clickedNumVal)) {
+//       // console.log(clickedNumVal);
+//       display = clickedNumVal;
+//       // console.log(display);
+//       readoutEl.value = display;
+//       if (a != undefined) {
+//         b = clickedNumVal;
+//         result = operate(clickedFunction, a, b);
+//       }
+//     }
+//   }
+//   if (clickedEl.classList.contains("calc-operation-btn")) {
+//     a = display;
+//     console.log(display);
+//     clickedFunction = e.target.getAttribute("data-value");
+//     console.log(clickedFunction);
+//     if (clickedFunction == "clear") {
+//       display = 0;
+//       readoutEl.value = display;
+//       a = undefined;
+//       b = undefined;
+//     }
+//     if (a !== undefined && b !== undefined) {
+//       readoutEl.value = result;
+//       console.log(result);
+//       if (clickedFunction == "=") {
+//         let docFrag = new DocumentFragment;
+//         let newEntryLine = document.createElement("span");
+//         newEntryLine.classList.add("tape-line");
+//         let newEntry = document.createTextNode(`${a} ${clickedFunction} ${b} = ${result}
+//         `);
+//         newEntryLine.appendChild(newEntry);
+//         docFrag.appendChild(newEntryLine);
+//         tape.appendChild(docFrag);
+//         a = undefined;
+//         b = undefined;
+//       }
+//     }
+//   }
+// });
+
